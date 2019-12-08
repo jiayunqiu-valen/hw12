22804fdb342d45c7facf4a61b8cd561ccc4ec16d
jiayunqiu-valen <55211937+jiayunqiu-valen@users.noreply.github.com>
2019-12-08 11:43:51 -0800

    Update robot.ino

--- robot.ino
+++ robot.ino
@@ -1,52 +1,68 @@
 #include <Adafruit_MotorShield.h>
-#define trigPin 8
-#define echoPin 7
+#define Trigpin 8
+#define Echopin 7
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 
-// The DC motor connected to M1
 Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
 
-// The DC motor connected to M2
 Adafruit_DCMotor *rightMotor = AFMS.getMotor(3);
+
+float distance;
+//car
+int flag = 0;
 
 void setup() {
   Serial.begin(19200);
   AFMS.begin();
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  leftMotor->setSpeed(200);
-  rightMotor->setSpeed(200);
+  pinMode(Trigpin, OUTPUT);
+  pinMode(Echopin, INPUT);
+  leftMotor->setSpeed(100);
+  rightMotor->setSpeed(100);
   Serial.println("Startup");
 }
 
-
-void loop() {
-   
-   long duration, distance; 
-  digitalWrite(trigPin, LOW);  
-  delayMicroseconds(2); 
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  duration = pulseIn(echoPin, HIGH);
-  distance = (duration/2) / 29.1;
- 
- delay(100);
-
-
-  if (distance < 20)
- 
-
- {   
-  Serial.print("turn");
+void go()
+{
   leftMotor->run(FORWARD);
-  rightMotor->run(BACKWARD);
-
+  rightMotor->run(FORWARD);
 }
 
- else {
-  Serial.print("Moving forward...");
+void back()
+{
+  leftMotor->run(BACKWARD);
+  rightMotor->run(BACKWARD);
+}
+
+void right()
+{
   leftMotor->run(FORWARD);
-  rightMotor->run(BACKWARD); 
-  }  
+  rightMotor->run(BACKWARD);
+}
+
+void csb()
+{
+  int dat;
+  digitalWrite(Trigpin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(Trigpin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(Trigpin, LOW);
+  distance = pulseIn(Echopin, HIGH) / 58.00;
+  Serial.print(distance);
+  Serial.println("cm");
+  dat = distance;
+  if (dat > 10) 
+  {
+    right();
+    
+  }
+  if (dat < 10) 
+  {
+   
+    back();
+  }
+}
+
+void loop() {
+  csb();
 }
